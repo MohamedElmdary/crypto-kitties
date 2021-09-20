44887fcfa28c9f7a256b188066daaae11ab0d4e0
MohamedElmdary <engm5081@gmail.com>
2021-09-20 14:04:38 +0200

    Added color controls

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ui';
+  public colors = this.fb.group({
+    body: [17],
+    mouth: [17],
+    ear: [18],
+    eye: [15]
+  });
+
+  get body(): FormControl {
+    return this.colors.get('body') as FormControl;
+  }
+
+  get mouth(): FormControl {
+    return this.colors.get('mouth') as FormControl;
+  }
+
+  get ear(): FormControl {
+    return this.colors.get('ear') as FormControl;
+  }
+
+  get eye(): FormControl {
+    return this.colors.get('eye') as FormControl;
+  }
+
+  constructor(private fb: FormBuilder) {}
 }
--- ui/src/app/cat/cat.component.ts
+++ ui/src/app/cat/cat.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { colors } from '../colors';
 
 @Component({
   selector: 'app-cat',
   templateUrl: './cat.component.html',
   styleUrls: ['./cat.component.scss']
 })
-export class CatComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CatComponent {
+  
+  bodyAndHead?: string;
+  @Input()
+  set body(key: keyof typeof colors) {
+    this.bodyAndHead = '#' + colors[key];
   }
 
+  mouthAndTail?: string;
+  @Input()
+  set mouth(key: keyof typeof colors) {
+    this.mouthAndTail = '#' + colors[key];
+  }
+
+  earAndPaw?: string;
+  @Input()
+  set ear(key: keyof typeof colors) {
+    this.earAndPaw = '#' + colors[key];
+  }
+
+  eyes?: string;
+  @Input()
+  set eye(key: keyof typeof colors) {
+    this.eyes = '#' + colors[key];
+  }
 }
+

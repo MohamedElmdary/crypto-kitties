c77d495383775558795af852b0faaf7b7a4d94f5
MohamedElmdary <engm5081@gmail.com>
2021-09-20 12:44:22 +0200

    Added main cat style

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatComponent } from './cat/cat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatComponent
   ],
   imports: [
     BrowserModule

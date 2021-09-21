e6b17858b5598aaf908e1a6369f0cde0c53078b3
MohamedElmdary <engm5081@gmail.com>
2021-09-21 09:21:54 +0200

    Added some shape variation

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,33 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  public colors = this.fb.group({
-    body: [17],
-    mouth: [17],
-    ear: [18],
-    eye: [15]
+  public catForm = this.fb.group({
+    body: [10],
+    mouth: [13],
+    eye: [96],
+    ear: [10],
+    eyeShape: [1],
+    decorationShape: [0]
   });
 
   get body(): FormControl {
-    return this.colors.get('body') as FormControl;
+    return this.catForm.get('body') as FormControl;
   }
 
   get mouth(): FormControl {
-    return this.colors.get('mouth') as FormControl;
+    return this.catForm.get('mouth') as FormControl;
   }
 
   get ear(): FormControl {
-    return this.colors.get('ear') as FormControl;
+    return this.catForm.get('ear') as FormControl;
   }
 
   get eye(): FormControl {
-    return this.colors.get('eye') as FormControl;
+    return this.catForm.get('eye') as FormControl;
+  }
+
+  get eyeShape(): FormControl {
+    return this.catForm.get('eyeShape') as FormControl;
+  }
+
+  get decorationShape(): FormControl {
+    return this.catForm.get('decorationShape') as FormControl;
   }
 
   constructor(private fb: FormBuilder) {}
--- ui/src/app/cat/cat.component.ts
+++ ui/src/app/cat/cat.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { colors } from '../colors';
 
 @Component({
   selector: 'app-cat',
   templateUrl: './cat.component.html',
-  styleUrls: ['./cat.component.scss']
+  styleUrls: ['./cat.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CatComponent {
   
@@ -26,10 +27,59 @@ export class CatComponent {
     this.earAndPaw = '#' + colors[key];
   }
 
-  eyes?: string;
-  @Input()
-  set eye(key: keyof typeof colors) {
-    this.eyes = '#' + colors[key];
+  @Input() eye!: keyof typeof colors;
+  @Input() eyeShape!: number;
+  get eyeStyle() {
+    let border: any;
+
+    switch (this.eyeShape) {
+      case 1:
+        border = {borderTop: '15px solid #333'};
+        break;
+
+      case 2:
+        border = {borderBottom: '15px solid #333'};
+        break;
+
+      default:
+        border = {};
+    }
+
+    return {
+      backgroundColor: this.eye ? '#' + colors[this.eye] : undefined,
+      ...border
+    }
+  }
+
+  @Input() decorationShape!: number;
+  get decorationStyle() {
+    let style: any;
+
+    switch (this.decorationShape) {
+      case 1:
+        const s = {display: 'none'};
+        style = {left: s, right: s};
+        break;
+
+      case 2:
+        style = {
+          left: { transform: 'rotate(20deg)' },
+          right: { transform: 'rotate(-20deg) '}
+        };
+        break;
+
+      case 3:
+        style = {
+          left: { transform: 'rotate(-20deg)' },
+          right: { transform: 'rotate(20deg) '}
+        };
+        break;
+
+      default:
+        style = {};
+    }
+
+    return style;
   }
 }
 
